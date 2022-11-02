66efcd95224abcf0de5ac294056bea29c227a657
Botashwin <117279303+Botashwin@users.noreply.github.com>
2022-11-02 18:50:53 +0530

    Add files via upload

--- mainapp.py
+++ mainapp.py
@@ -8,7 +8,7 @@ import tkinter as tk
 import sys
 from tkinter import *
 from tkinter import messagebox
-import maincalc,mainmusic,mainrps
+import maincalc,mainmusic,mainrps,mainwordle
 from PIL import Image, ImageTk
 
 print("\n---------------------------------------------------------------")
@@ -25,9 +25,11 @@ def calc():
     #screen.deiconify()
 def music():
     mainmusic.main()
-def rps():
+def rps1():
     mainrps.main()
-def exit():
+def wordle1():
+    mainwordle.main()
+def exit1():
     if messagebox.askokcancel("Exit","Do You Want To Quit? 🥺 "):
         screen.destroy()
         print("\nThank You For Using The Ultra App X .... ¯\_(ツ)_/¯")
@@ -35,7 +37,7 @@ def exit():
 
 screen = tk.Tk()
 screen.title('The Ultra App X')
-'''screen.iconbitmap(r')'''
+screen.iconbitmap(r'xicon.ico')
 screen.geometry("900x400")
 screen.iconbitmap(r'xicon.ico')
 screen.configure(bg="black")
@@ -54,6 +56,19 @@ img2 = ImageTk.PhotoImage(Image.open("musicplay.png"))
 label2 = Label(frame2, image = img2)
 label2.pack()
 
+frame3 = Frame(screen, width=140, height=140)
+frame3.pack()
+frame3.place(x=510,y=80)
+img3 = ImageTk.PhotoImage(Image.open("rps.png"))
+label3 = Label(frame3, image = img3)
+label3.pack()
+
+frame4 = Frame(screen, width=140, height=140)
+frame4.pack()
+frame4.place(x=710,y=80)
+img4 = ImageTk.PhotoImage(Image.open("wordle.png"))
+label4 = Label(frame4, image = img4)
+label4.pack()
 
 calculator = Button(screen, text="Calculator", command=calc)
 calculator.config(font=('Trebuchet MS', 20), highlightbackground="#FE0221", bg="white", fg="black", padx=7, pady=7)
@@ -63,7 +78,15 @@ musicplayer = Button(screen, text="Music Player", command=music)
 musicplayer.config(font=('Trebuchet MS', 20), highlightbackground="#FAFE02", bg="white", fg="black", padx=7, pady=7)
 musicplayer.place(x=280, y=250)
 
-Exitbtn = Button(screen, text="Exit", command=exit)
+rps = Button(screen, text="RPS", command=rps1)
+rps.config(font=('Trebuchet MS', 20), highlightbackground="#1DFE02", bg="white", fg="black", padx=7, pady=7)
+rps.place(x=545, y=250)
+
+wordle = Button(screen, text="Wordle", command=wordle1)
+wordle.config(font=('Trebuchet MS', 20), highlightbackground="#DF02FE", bg="white", fg="black", padx=7, pady=7)
+wordle.place(x=720, y=250)
+
+Exitbtn = Button(screen, text="Exit", command=exit1)
 Exitbtn.config(font=('Trebuchet MS', 20), highlightbackground="#02FEFA", bg="white", fg="black", padx=7, pady=7)
 Exitbtn.place(x=410, y=336)
 
